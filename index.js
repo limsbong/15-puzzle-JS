9f62871d98bd3c1d0ddc5fc4c57c4e9aefcfcc29
limshyun <96limshyun@gmail.com>
2023-11-14 23:14:03 +0900

    [Fixes] 오버플로우 문제 수정중

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
 const getRandomNumber = () => {
-    let numbers = [];
-    while (numbers.length !== 8) {
-        let randomNumber = Math.floor(Math.random() * (9 - 1)) + 1;
-        if (!numbers.includes(randomNumber)) {
-            numbers.push(randomNumber);
+    let randomNumbers = [];
+    while (randomNumbers.length !== 16) {
+        let number = Math.floor(Math.random() * (16 - 0)) + 0;
+        if (!randomNumbers.includes(number)) {
+            randomNumbers.push(number);
         } else {
             continue;
         }
     }
-    return numbers;
+    
+    randomNumbers.forEach((cur, idx) => {
+        if (cur === 0) {
+            randomNumbers.splice(idx, 1, " ")
+        } else if (cur < 10) {
+            randomNumbers.slice(idx, 1, " " + cur)
+        }
+    })
+    return randomNumbers;
 }
 
 let userTurn = 0;
-let numbers = getRandomNumber();
+const numbers = getRandomNumber();
 
 const printNumber = () => {
-    console.log("간단 숫자 퍼즐");
-    console.log(`Turn ${userTurn}`);
-    console.log(numbers);
-    console.log("교활할 두 숫자를 입력하세요.");
+    console.log("재미있는 15 퍼즐!");
+    console.log(`Turn: ${userTurn}`);
+    for (let i = 0; i < 13; i += 4) {
+        console.log(`[${numbers[i]}][${numbers[i + 1]}][${numbers[i + 2]}][${numbers[i + 3]}]`);
+    }
+    console.log("숫자를 입력하세요.");
     userTurn++;
 }
 
 const isNumberSort = () => {
-    for (let i = 1; i <= 8; i++) {
+    for (let i = 1; i <= 15; i++) {
         if (i === numbers[i - 1]) {
             continue;
         } else {
@@ -34,35 +44,52 @@ const isNumberSort = () => {
 }
 
 const valueCheck = (input) => {
-    input.forEach((cur) => {
-        if (input.length === 1 || cur > 8 || isNaN(cur)) {
-            alert("잘 못 입력하였습니다. 다시 입력해주세요.")
-            main()
-        } else {
-            return input;
-        }
-    })
+    if (input > 15 || isNaN(input)) {
+        alert("잘 못 입력하였습니다. 다시 입력해주세요.")
+        main()
+    } else {
+        return true;
+    }
+}
+
+const moveNumber = (number) => {
+    const userNumIdx = numbers.indexOf(number);
+    const emptyIdx = numbers.indexOf(" ");
+    
+    if (userNumIdx + 4 === emptyIdx || userNumIdx - 4 === emptyIdx) {
+        swapNum(userNumIdx, emptyIdx);
+    } else if (userNumIdx + 1 === emptyIdx || userNumIdx - 1 === emptyIdx) {
+        swapNum(userNumIdx, emptyIdx);
+    } else {
+        alert("다른 숫자를 입력해주세요");
+        main();
+    }
+}
+
+const swapNum = (userNumIdx, emptyIdx) => {
+    const temp = numbers[userNumIdx];
+    numbers.splice(userNumIdx, 1, numbers[emptyIdx]);
+    numbers.splice(emptyIdx, 1, temp);
+    
+    return true;
 }
 
 const main = () => {
     let isAlive = false;
     while (!isAlive) {
         printNumber();
-        let input = prompt("두 숫자를 입력하세요 (예: 1,2)").split(",");
-        console.log(input);
-        valueCheck(input);
-        let firstNum = parseInt(input[0]);
-        let secondNum = parseInt(input[1]);
-        let firstNumIdx = numbers.indexOf(firstNum);
-        let secondNumIdx = numbers.indexOf(secondNum);
-        let temp = numbers[firstNumIdx];
-        numbers.splice(firstNumIdx, 1, numbers[secondNumIdx]);
-        numbers.splice(secondNumIdx, 1, temp);
-        isAlive = isNumberSort();
+        
+    //     const input = prompt("숫자를 입력해주세요 (예: 1)");
+    //     const number = parseInt(input);
+        
+    //     valueCheck(number);
+        
+    //     moveNumber(number);
+        
+    //     isAlive = isNumberSort();
     }
     printNumber();
     alert("정렬되었습니다.");
 }
 
-main()
-
+// main();
