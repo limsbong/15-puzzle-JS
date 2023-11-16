f4cd0091f394c9fdba04d9189ebcdf9c55a7feed
limshyun <96limshyun@gmail.com>
2023-11-16 19:55:04 +0900

    [v2] 2단계: 15퍼즐 구현하기 완료

--- index.js
+++ index.js
@@ -1,65 +1,60 @@
 const getRandomNumber = () => {
     let randomNumbers = [];
     while (randomNumbers.length !== 16) {
-        let number = Math.floor(Math.random() * (16 - 0)) + 0;
+        const number = Math.floor(Math.random() * (16 - 0)) + 0;
         if (!randomNumbers.includes(number)) {
             randomNumbers.push(number);
         } else {
             continue;
         }
     }
-    
+
     randomNumbers.forEach((cur, idx) => {
         if (cur === 0) {
             randomNumbers.splice(idx, 1, " ")
-        } else if (cur < 10) {
-            randomNumbers.slice(idx, 1, " " + cur)
         }
     })
     return randomNumbers;
 }
 
-let userTurn = 0;
+let cnt = 0;
 const numbers = getRandomNumber();
+const answerArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, " "];
 
 const printNumber = () => {
-    console.log("재미있는 15 퍼즐!");
-    console.log(`Turn: ${userTurn}`);
-    for (let i = 0; i < 13; i += 4) {
-        console.log(`[${numbers[i]}][${numbers[i + 1]}][${numbers[i + 2]}][${numbers[i + 3]}]`);
-    }
-    console.log("숫자를 입력하세요.");
-    userTurn++;
+    const userTurnCntText = document.querySelector("#user-turn");
+    const container = document.querySelector("#main-contain").children;
+    numbers.forEach((cur, idx) => {
+        container[idx].textContent = cur;
+    })
+
+    userTurnCntText.textContent = `${cnt} 번째`;
 }
 
 const isNumberSort = () => {
-    for (let i = 1; i <= 15; i++) {
-        if (i === numbers[i - 1]) {
+    for (let i = 0; i <= 15; i++) {
+        if (answerArray[i] === numbers[i]) {
             continue;
         } else {
-            main()
+            return false;
         }
     }
+    printNumber()
     return true;
 }
 
-const valueCheck = (input) => {
-    if (input > 15 || isNaN(input)) {
-        alert("잘 못 입력하였습니다. 다시 입력해주세요.")
-        main()
-    } else {
-        return true;
-    }
-}
-
-const moveNumber = (number) => {
-    const userNumIdx = numbers.indexOf(number);
+const moveNumber = (userSelectNum) => {
+    const userNumIdx = numbers.indexOf(userSelectNum);
     const emptyIdx = numbers.indexOf(" ");
-    
+
     if (userNumIdx + 4 === emptyIdx || userNumIdx - 4 === emptyIdx) {
         swapNum(userNumIdx, emptyIdx);
+        cnt++;
+        printNumber()
     } else if (userNumIdx + 1 === emptyIdx || userNumIdx - 1 === emptyIdx) {
         swapNum(userNumIdx, emptyIdx);
+        cnt++;
+        printNumber()
     } else {
         alert("다른 숫자를 입력해주세요");
         main();
@@ -70,26 +65,26 @@ const swapNum = (userNumIdx, emptyIdx) => {
     const temp = numbers[userNumIdx];
     numbers.splice(userNumIdx, 1, numbers[emptyIdx]);
     numbers.splice(emptyIdx, 1, temp);
-    
-    return true;
+}
+
+const startGame = (event) => {
+    let isAlive = false;
+    const userSelectNum = Number(event.target.textContent);
+    moveNumber(userSelectNum);
+
+    isAlive = isNumberSort();
+    if (isAlive) {
+        const endModal = document.querySelector("#end-modal");
+        const endText = document.querySelector("#sorted");
+        endText.textContent = `${cnt}번 만에 정렬되었습니다!`
+        endModal.style.display = "flex";
+    }
 }
 
 const main = () => {
-    let isAlive = false;
-    while (!isAlive) {
-        printNumber();
-        
-    //     const input = prompt("숫자를 입력해주세요 (예: 1)");
-    //     const number = parseInt(input);
-        
-    //     valueCheck(number);
-        
-    //     moveNumber(number);
-        
-    //     isAlive = isNumberSort();
-    }
     printNumber();
-    alert("정렬되었습니다.");
+    const containerSelect = document.querySelector("#main-contain");
+    containerSelect.addEventListener("click", startGame);
 }
 
-// main();
+main();
